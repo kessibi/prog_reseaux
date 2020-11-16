bb9643868230f569207ddd569044b8756fddc5c4
Guillaume <guillaume@kessibi.fr>
2020-11-16 15:20:35 +0100

    response: now handles multiple file encoding

--- TP2/TP-HTTP-Code/src/http/server/Response.java
+++ TP2/TP-HTTP-Code/src/http/server/Response.java
@@ -2,6 +2,7 @@ package http.server;
 
 import http.server.header.Header;
 import java.io.IOException;
+import java.nio.charset.MalformedInputException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,11 +21,20 @@ public class Response {
 
   public void findFile() throws IOException {
     String fileName = this.resHeader.getFileName();
+    Path filePath;
 
     try {
       // TODO ../www is not the cleanest way to do this
-      System.out.println(fileName);
-      Path filePath = Path.of("../www" + fileName);
+      filePath = Path.of("../www" + fileName);
+      this.resPayload = Files.readString(filePath, StandardCharsets.UTF_8);
+
+      this.resHeader.setMime(Files.probeContentType(filePath));
+      this.resHeader.setCode(200);
+      this.resHeader.setLength(this.resPayload.length());
+
+    } catch (MalformedInputException mie) {
+      // in case a file is not UTF-8 encoded (eg: favicon.ico)
+      filePath = Path.of("../www" + fileName);
       this.resPayload = Files.readString(filePath, StandardCharsets.ISO_8859_1);
 
       this.resHeader.setMime(Files.probeContentType(filePath));
