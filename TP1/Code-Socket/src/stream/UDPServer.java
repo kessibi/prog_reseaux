0c1f6562fa24c75c25b3cbdb2980378d0c8cc54b
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-11-18 10:51:55 +0100

    udp basicly works

--- TP1/Code-Socket/src/stream/UDPServer.java
+++ TP1/Code-Socket/src/stream/UDPServer.java
@@ -13,27 +13,41 @@ public class UDPServer {
    public static void main(String args[]) {
      int argInt = Integer.parseInt(args[0]);
      int argLength = args.length;
-     EchoServerMultiThreaded serveur = new UDPServer(argInt, argLength);
+     UDPServer serveur = new UDPServer(argInt, argLength);
 
    }
 
-   public UDPServer (int argInt, int argLength) {
-	   history = new History();
-	   // history.printAllMessages();
+   public UDPServer (int port, int argLength) {
 	 int threadId = 0;
      if (argLength != 1) {
        System.out.println("Usage: java EchoServer <EchoServer port>");
        System.exit(1);
      }
      try {
-
-       }
+         DatagramSocket serverSock = new DatagramSocket(port);
+         while (true) {
+           System.out.println("Waiting for client packet...");
+           byte[] buf = new byte[256];
+           // Create a datagram packet
+           DatagramPacket packet = new DatagramPacket(buf, buf.length);
+           // Wait for a packet
+           serverSock.receive(packet);
+           // Get client IP address and port number
+           InetAddress clientAddr = packet.getAddress();
+           int clientPort = packet.getPort();
+           // Build a response
+           //initialize buf ...
+           // Build a datagram packet for response
+           packet = new DatagramPacket(buf, buf.length, clientAddr, clientPort);
+           // Send a response
+           serverSock.send(packet);
+         }
      } catch (Exception e) {
        System.err.println("Error in EchoServer:" + e);
      }
 
    }
 
-  
+
 
 }
