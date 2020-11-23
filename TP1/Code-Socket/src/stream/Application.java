4c3bc0f1fa886a178ed64e75fd707cb7593d25e4
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-11-23 09:15:26 +0100

    add ui

---
+++ TP1/Code-Socket/src/stream/Application.java
@@ -0,0 +1,12 @@
+package stream;
+
+import java.io.IOException;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException {
+	    System.out.println("Lancement de l'application");
+	    ChatWindow fenetre = new ChatWindow ();
+	  }
+	
+}
