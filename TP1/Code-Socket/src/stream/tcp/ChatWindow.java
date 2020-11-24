41fe9a2936f45461f5d7ce1084b8674508e9acb1
Paul Bodelot <49484206+paulbodelot@users.noreply.github.com>
2020-11-24 17:18:07 +0100

    remove security

--- TP1/Code-Socket/src/stream/tcp/ChatWindow.java
+++ TP1/Code-Socket/src/stream/tcp/ChatWindow.java
@@ -143,7 +143,6 @@ public class ChatWindow extends JFrame implements KeyListener, ActionListener {
         deconnexion.setEnabled(true);
         connexion.setEnabled(false);
         estConnecte();
-        this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
       } catch (UnknownHostException e) {
         System.err.println("Don't know about host:" + ip_nom);
         estErreur();
